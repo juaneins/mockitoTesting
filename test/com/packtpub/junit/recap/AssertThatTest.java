ce321e248b625c9a419f2bde6c587d90117522c4
juaneins <ballesterosjg@gmail.com>
2020-11-05 21:58:56 -0500

    Working with compound value matchers – either,both, anyOf, allOf, and not

--- test/com/packtpub/junit/recap/AssertThatTest.java
+++ test/com/packtpub/junit/recap/AssertThatTest.java
@@ -5,19 +5,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.equalTo;
-
+import static org.hamcrest.CoreMatchers.either;
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.allOf;
 
 public class AssertThatTest {
-	
+
 	@Test
-	public void verifyMatcher() {
+	public void verifyMatcherValues() {
 		int age = 30;
 		assertThat(age, equalTo(30));
 		assertThat(age, is(30));
-		
+
 		assertThat(age, not(equalTo(33)));
 		assertThat(age, is(not(33)));
 	}
 
-}
+	@Test
+	public void verifyMultipleValues() {
+		double marks = 100.00;
+		assertThat(marks, either(is(100.00)).or(is(90.9)));
+		assertThat(marks, both(not(99.99)).and(not(60.00)));
+		assertThat(marks, anyOf(is(100.00), is(1.00), is(55.00), is(88.00), is(67.8)));
+		assertThat(marks, not(anyOf(is(0.00), is(200.00))));
+		assertThat(marks, not(allOf(is(1.00), is(100.00), is(30.00))));
 
+	}
+
+}
